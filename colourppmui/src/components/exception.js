d3c7c8b4dcfc2802f7d7d0b5ab3d499018f8e3b0
William Claydon <will@heisamachine.com>
2020-04-11 16:10:26 +1000

    exceptions are now buttons that work
    Signed-off-by: William Claydon <will@heisamachine.com>

--- colourppmui/src/components/exception.js
+++ colourppmui/src/components/exception.js
@@ -1,58 +1,84 @@
 import React from "react";
-import {
-  List,
-  Datagrid,
-  ReferenceField,
-  TextField
-} from "react-admin";
+import { List, Datagrid, ReferenceField, TextField } from "react-admin";
 
-import Button from '@material-ui/core/Button';
-import { Link } from 'react-router-dom';
+import Button from "@material-ui/core/Button";
+import { Link } from "react-router-dom";
 
 const CreateRelatedCommentButton = ({ item }) => (
   <Button
-      component={Link}
-      to={{
-          pathname: '/allocation',
-          state: item ? { filter: { id: item.id } } : {},
-      }}
+    component={Link}
+    to={{
+      pathname: "/allocation",
+      state: item ? { filter: { id: item.id } } : {}
+    }}
   >
-      {item ? item.allocation : 0}
+    {item ? item.allocation : 0}
   </Button>
 );
 
 const TagsField = ({ record }) => (
   <div>
-      {record.allocations ? <table> <tr>{ record.allocations.map(item => (
-          <th>{item.month}</th>
-      )) }  </tr> <tr>{ record.allocations.map(item => (
-        <th style={item.allocation > 1 ? {color:"red"} :{color:"green"}}>{item ? item.allocation : 0}</th>
-    )) }</tr> </table> : <div>No allocations</div>}
+    {record.allocations ? (
+      <table>
+        {" "}
+        <tr>
+          {record.allocations.map(item => (
+            <th>{item.month}</th>
+          ))}{" "}
+        </tr>{" "}
+        <tr>
+          {record.allocations.map(item => (
+            <th
+              style={
+                item.allocation > 1 ? { color: "red" } : { color: "green" }
+              }
+            >
+              {item ? (
+                <Button
+                  color={item.allocation > 1 ? "secondary" : "primary"}
+                  component={Link}
+                  to={{
+                    pathname: "/allocation",
+                    search:
+                      '?filter=%7B"id"%3A' +
+                      encodeURIComponent(JSON.stringify(item.id)) +
+                      "%7D"
+                  }}
+                >
+                  {item ? item.allocation.toFixed(2) : "No Alloc"}
+                </Button>
+              ) : (
+                0
+              )}
+            </th>
+          ))}
+        </tr>{" "}
+      </table>
+    ) : (
+      <div>No allocations</div>
+    )}
   </div>
-)
+);
 TagsField.defaultProps = { addLabel: true };
 
 const EditEntityButton = ({ record }) => (
   <Button
-      component={Link}
-      to={{
-          pathname: `/entity/${record.id}/show`,
-      }}
+    component={Link}
+    to={{
+      pathname: `/entity/${record.id}/show`
+    }}
   >
-      Edit
+    Edit
   </Button>
 );
 
 export const ExceptionList = props => (
   <List {...props}>
-    <Datagrid >
-    <ReferenceField source="id" reference="entity" >
+    <Datagrid>
+      <ReferenceField source="id" reference="entity">
         <TextField source="name" />
       </ReferenceField>
-      
-     
       <TagsField source="allocations"></TagsField>
-
       <EditEntityButton />
     </Datagrid>
   </List>
