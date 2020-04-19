f253273d3b7917282d92ad588eda3e5205b1d048
William Claydon <will@heisamachine.com>
2020-04-19 10:43:43 +1000

    use soft delete for entity

--- colourppmui/src/components/entity.js
+++ colourppmui/src/components/entity.js
@@ -17,10 +17,41 @@ import {
   SimpleForm,
   TextInput,
   Filter,
-  Pagination
+  Pagination,
+  Toolbar,
+  SaveButton
 } from "react-admin";
 import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+
 import { Link } from 'react-router-dom';
+
+import DeleteWithUndoButton from './DeleteWithUndoButton'
+const useStyles = makeStyles({
+  toolbar: {
+      display: 'flex',
+      justifyContent: 'space-between',
+  },
+});
+
+const ToolbarCustomButton = ({
+  handleSubmit,
+  handleSubmitWithRedirect,
+  onSave,
+  invalid,
+  pristine,
+  saving,
+  submitOnEnter,
+  ...rest
+}) => <DeleteWithUndoButton {...rest} />;
+
+const CustomToolbar = ({ permissions, ...props }) => 
+  <Toolbar {...props} classes={useStyles()}>
+      <SaveButton />
+{permissions === 'su' && <ToolbarCustomButton  {...props}/> }
+  </Toolbar>
+  ;
+
 
 const AssoicationButton = () => (
   <Button color="secondary" variant="outlined"
@@ -98,9 +129,9 @@ const EntityTitle = ({ record }) => {
   return <span>Entity {record ? `"${record.name}"` : ""}</span>;
 };
 
-export const EntityEdit = props => (
+export const EntityEdit = ({ permissions, ...props }) => (
   <Edit title={<EntityTitle />} {...props}>
-    <SimpleForm>
+    <SimpleForm toolbar={<CustomToolbar permissions={permissions}  />}>
       <TextInput source="id" disabled />
       <ReferenceInput
         label="Entity Type"
